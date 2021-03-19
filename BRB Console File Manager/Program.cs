992f37acb1a8db50993b66af6271757ed6220af4
Alexey Zhurkov <doxe@yandex.ru>
2021-03-19 09:40:00 +0300

    First code added

---
+++ BRB Console File Manager/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace BRB_Console_File_Manager
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Title = "BRB Console File Manager";
+            DrawInterface();
+            while (true)
+            {
+                KeyPress(Console.ReadKey(true));
+            }
+        }
+
+        private static void DrawInterface()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.SetCursorPosition(0, 0);
+            Console.Write('╔');  //╚ ╝ ║ ═ ╗ ╔ ► ◄
+            Console.SetCursorPosition(Console.WindowHeight-1, Console.WindowWidth-1);
+            Console.Write('╝');
+            Console.SetCursorPosition(Console.WindowHeight-1, 0);
+            Console.Write('╗');
+            Console.SetCursorPosition(0, Console.WindowWidth-1);
+            Console.Write('╚');
+            for (int i=1;i<Console.WindowWidth-2;i++)
+            {
+                Console.SetCursorPosition(i, 0);
+                Console.Write('═');
+                Console.SetCursorPosition(i, Console.WindowHeight-1);
+                Console.Write('═');
+            }
+            for (int i=1;i<Console.WindowHeight-2;i++)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.Write('║');
+                Console.SetCursorPosition(Console.WindowWidth-1, i);
+                Console.Write('║');
+            }
+        }
+
+        private static void KeyPress(ConsoleKeyInfo consoleKeyInfo)
+        {
+            switch (consoleKeyInfo.Key)
+            {
+                
+            }
+        }
+    }
+}
